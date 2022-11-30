8ca3285dbad112797b9fe2721a9ae30b2181569d
Charlotte Foley <cvfoley@umich.edu>
2022-11-30 12:48:31 -0500

    change

--- discussion_12.py
+++ discussion_12.py
@@ -16,7 +16,10 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
-    pass
+    cur.execute("CREATE TABLE IF NOT EXISTS employees (employee_id INTERGER PRIMARY KEY, first_name TEXT, last_name TEXT, hire_date TEXT, job_id INTEGER, salary INTEGER")
+    conn.commit()
+        
+
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
 
@@ -27,10 +30,32 @@ def add_employee(filename, cur, conn):
     file_data = f.read()
     f.close()
     # THE REST IS UP TO YOU
+    data = json.loads(file_data)
+    for item in data:
+        id = item['employee_id']
+        first_name = item['first_name']
+        last_name = item['last_name']
+        date = item['hire_date']
+        job_id = item['job_id']
+        salary = item['salary']
+        cur.execute(
+            '''
+            INSERT OR IGNORE INTO employees (employee_id, first_name, last_name, job_id, hire_date, salary), VALUES (?,?,?,?,?,?)
+            '''
+            (id, first_name, last_name, date, job_id, salary)
+        )
+    conn.commit()
+
     pass
 
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
+    cur.execute(
+        '''
+        SELECT Employees
+        
+        '''
+    )
     pass
 
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
